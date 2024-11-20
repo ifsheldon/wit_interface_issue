e3b108410b905b8ef88eb1262b2c41d5083f537b
maple@max <feng.liang@kaust.edu.sa>
2024-11-20 20:54:20 +0800

    modify script and use nightly rustc

--- host/run.sh
+++ host/run.sh
@@ -1,5 +1,9 @@
 cd ../implementation
 echo "Building implementation..."
-cargo build --target wasm32-wasip2 --release
+cargo clean
+RUSTFLAGS="-C panic=abort" cargo build --target wasm32-wasip2 --release -Zbuild-std -Zbuild-std-features=panic_immediate_abort
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 cd -
 cargo run
